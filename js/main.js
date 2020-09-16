52180f78cff74f27c2da282c64a50d9ef8034971
Roberto Del Toro <robertodeltoro93@gmail.com>
2020-09-16 19:00:28 +0200

    Generazione griglia js/jquery

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
 
-  var boxes = $(".box");
+  //genera griglia
+  var row = "";
+  for (var i = 0; i < 6; i++){
+  	row += "<div class='box-row'></div>";
+  	for(var j = 0; j < 6; j++){
+  		row += "<div class='box'></div>";
+  	}
+  }
+  $('.box-wrapper').append(row);
 
+  var boxes = $(".box");
   boxes.click(function(){
-    
+
     //salvo il this in una variabile dato che dentro l'ajax il this non funziona
     var self = $(this);
     $.ajax(
