5c79f2dab985ab1d8a156ecf6e1b4839475e4249
Roberto Del Toro <robertodeltoro93@gmail.com>
2020-09-16 17:58:07 +0200

    Versione corretta Esercizio quadrati

--- js/main.js
+++ js/main.js
@@ -3,26 +3,25 @@ $(document).ready(function(){
   var boxes = $(".box");
 
   boxes.click(function(){
+    
+    //salvo il this in una variabile dato che dentro l'ajax il this non funziona
+    var self = $(this);
     $.ajax(
       {
         "url": "https://flynn.boolean.careers/exercises/api/random/int",
         "method": "GET",
         "success": function (data, stato) {
           var numeroServer = data.response;
-          console.log(numeroServer);
 
-          for(var i = 1; i <= boxes.length; i++){
-            var index = i - 1;
             if(numeroServer <= 5){
-              $(boxes[index]).addClass("yellow");
-              $(boxes[index]).removeClass("green");
-              $(boxes[index]).text(numeroServer);
+              self.addClass("yellow");
+              self.removeClass("green");
+              self.text(numeroServer);
             } else {
-              $(boxes[index]).addClass("green");
-              $(boxes[index]).removeClass("yellow");
-              $(boxes[index]).text(numeroServer);
+              self.addClass("green");
+              self.removeClass("yellow");
+              self.text(numeroServer);
             }
-          }
 
         },
         "error": function (richiesta, stato, errori) {
